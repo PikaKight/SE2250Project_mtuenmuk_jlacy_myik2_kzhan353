dd7e7d3d1b28ddeec33004cb0f47b2089c750935
PikaKight <tmmarcus73@gmail.com>
2022-04-05 20:40:01 -0400

    got the menu working

---
+++ RPG-Unity/Assets/__Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class MainMenu : MonoBehaviour
+{   
+    public GameObject nextScreen, player; 
+    
+    public void PlayGame(){
+        this.gameObject.SetActive(false);
+        nextScreen.SetActive(true);
+    }
+
+    public void CharSelect(Sprite character){
+        PlayGame();
+        player.GetComponent<SpriteRenderer>().sprite = character;
+    }
+}
